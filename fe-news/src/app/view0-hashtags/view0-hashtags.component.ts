c93d6710d8269bc6376b5eebc5ad5f498bad4f02
Nigel Tay <82434359+nigel-tay@users.noreply.github.com>
2023-08-31 14:27:47 +0800

    add time form

--- fe-news/src/app/view0-hashtags/view0-hashtags.component.ts
+++ fe-news/src/app/view0-hashtags/view0-hashtags.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-view0-hashtags',
   templateUrl: './view0-hashtags.component.html',
   styleUrls: ['./view0-hashtags.component.scss']
 })
-export class View0HashtagsComponent {
+export class View0HashtagsComponent implements OnInit{
+  timeArray: string[] = ['5', '15', '30', '45', '60'];
+  timeForm!: FormGroup;
+
+  constructor(private fb: FormBuilder){ }
+
+  ngOnInit(): void {
+    this.timeForm = this.fb.group({
+      minutes: this.fb.control(this.timeArray[0])
+    });
+  }
+
 
 }
