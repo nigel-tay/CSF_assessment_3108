6eae3b019c3365b6383960be08049b0b2dd348ae
Nigel Tay <82434359+nigel-tay@users.noreply.github.com>
2023-08-31 12:34:04 +0800

    tidy up imageRepo

--- news-server/src/main/java/vttp2023/batch3/csf/assessment/cnserver/repositories/ImageRepository.java
+++ news-server/src/main/java/vttp2023/batch3/csf/assessment/cnserver/repositories/ImageRepository.java
@@ -17,17 +17,6 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 
 @Repository
 public class ImageRepository {
-	@Value("${s3.key.access}")
-	private String accessKey;
-
-	@Value("${s3.key.secret}")
-	private String secretKey;
-
-	@Value("${s3.bucket.endpoint}")
-	private String bucketEndpoint;
-
-	@Value("${s3.bucket.region}")
-	private String region;
 	
 	// TODO: Task 1
 	@Autowired
@@ -35,24 +24,11 @@ public class ImageRepository {
 
 	public String uploadFile(MultipartFile file) throws IOException{
 
-	//    Map<String, String> userData = new HashMap<>();
-	//    userData.put("firstName", firstName);
-	//    userData.put("lastName", lastName);
-	//    userData.put("phone", phone);
-	//    userData.put("comments", comments);
-
-	// System.out.println(accessKey + "       DOG        " + secretKey);
-
 		ObjectMetadata metadata = new ObjectMetadata();
 		metadata.setContentType(file.getContentType());
 		metadata.setContentLength(file.getSize());
 		String uuid = UUID.randomUUID().toString().substring(0,8);
 		InputStream is = file.getInputStream();
-
-		System.out.println(accessKey);
-		System.out.println(secretKey);
-		System.out.println(bucketEndpoint);
-		System.out.println(region);
 
 		PutObjectRequest putReq = new PutObjectRequest("fredbarney", uuid, is, metadata);
 		putReq = putReq.withCannedAcl(CannedAccessControlList.PublicRead);
