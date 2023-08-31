4d55773f871152e1cc6c9946dd9b3d2c0d43a54c
Nigel Tay <82434359+nigel-tay@users.noreply.github.com>
2023-08-31 12:33:36 +0800

    create json object

--- news-server/src/main/java/vttp2023/batch3/csf/assessment/cnserver/controllers/NewsController.java
+++ news-server/src/main/java/vttp2023/batch3/csf/assessment/cnserver/controllers/NewsController.java
@@ -1,6 +1,10 @@
 package vttp2023.batch3.csf.assessment.cnserver.controllers;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -11,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import vttp2023.batch3.csf.assessment.cnserver.models.News;
 import vttp2023.batch3.csf.assessment.cnserver.services.NewsService;
 
 @RestController
@@ -30,8 +37,20 @@ public class NewsController {
 		) throws IOException {
 			System.out.println(title.toString() + "  " + photo.toString() + "  " + description.toString() + "  " + tags.toString());
 			
-			System.out.println(nService.saveImageToS3(photo));
-		return null;
+			String uuid = nService.saveImageToS3(photo);
+
+			Date date = new Date();
+			List<String> tagsList = Arrays.asList(tags.split(",", -1));
+
+			JsonObject jo = Json.createObjectBuilder()
+								.add("postDate", date.getTime())
+								.add("title", title.toString())
+								.add("description", description.toString())
+								.add("image", "https://fredbarney.sgp1.cdn.digitaloceanspaces.com/"+uuid)
+								.add("tags", Json.createArrayBuilder(tagsList))
+								.build();
+
+		return ResponseEntity.ok(jo.toString());
 	}
 
 	// TODO: Task 2
