ac7b66cb2ce0ab92f517c27fc563e583d4433d08
Nigel Tay <82434359+nigel-tay@users.noreply.github.com>
2023-08-31 11:21:40 +0800

    add postFormToMongo method in newsController

--- news-server/src/main/java/vttp2023/batch3/csf/assessment/cnserver/controllers/NewsController.java
+++ news-server/src/main/java/vttp2023/batch3/csf/assessment/cnserver/controllers/NewsController.java
@@ -1,12 +1,26 @@
 package vttp2023.batch3.csf.assessment.cnserver.controllers;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class NewsController {
 
 	// TODO: Task 1
-
+	@PostMapping(path="/receiveform", consumes=MediaType.MULTIPART_FORM_DATA_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> postFormToMongo(
+		@RequestPart MultipartFile title,
+		@RequestPart MultipartFile photo,
+		@RequestPart MultipartFile description,
+		@RequestPart MultipartFile tags
+		) {
+			System.out.println(title.toString() + "  " + photo.toString() + "  " + description.toString() + "  " + tags.toString());
+		return null;
+	}
 
 	// TODO: Task 2
 
